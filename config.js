9ffdb6c438733334d61cc8333ea1a89966373a53
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2019-10-16 01:13:56 -0600

    Resume and Dynamic Rendering

---
+++ config.js
@@ -0,0 +1,12 @@
+var config=
+[
+  {
+    title:"E-Center",
+    content:"",
+    url:"",
+    github:"",
+    pkg:"",
+    stats:"",
+    images:["./assets/portfolio/ecenterhome.png","./assets/portfolio/ecenterstats.png"]
+  },
+];
